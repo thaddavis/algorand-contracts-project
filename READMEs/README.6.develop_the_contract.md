81ddd537a622aa616efac69b80f496298ae80e45
Your Name <you@example.com>
2022-12-10 19:20:15 -0500

    add some more features to the smoke test contract

---
+++ READMEs/README.6.develop_the_contract.md
@@ -0,0 +1,4 @@
+# Develop the contract
+
+- adding some storage
+- ./build.sh contracts.simple_contracts.smoke_test
--- contracts/simple_contracts/smoke_test.py
+++ contracts/simple_contracts/smoke_test.py
@@ -1,10 +1,24 @@
 from pyteal import *
+from pyteal_helpers import program
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).absolute().parent.parent))
 
+
 def approval():
-    return Approve()
+    global_owner = Bytes("owner")  # byteslice
+    global_counter = Bytes("counter")  # int
+
+    return program.event(
+        init=Seq(
+            [
+                App.globalPut(global_counter, Int(0)),
+                App.globalPut(global_owner, Txn.sender()),
+                Approve()
+            ]
+        )
+    )
+
 
 def clear():
     return Approve()
