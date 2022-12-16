cd8d7f1a42be51f2fa48cfcf7add0216103936ce
Your Name <you@example.com>
2022-12-16 17:47:42 -0500

    saved test setup and configured autopep8 to NOT re-order imports

---
+++ contracts/escrow/escrow.py
@@ -0,0 +1,12 @@
+from re import M
+from pyteal import *
+from pyteal.ast.bytes import Bytes
+from pyteal_helpers import program
+
+
+def approval():
+    return Approve()
+
+
+def clear():
+    return Approve()
